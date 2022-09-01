98d3fd6dacb90115610c65bc2ae89d80dec47c95
Prabagaran <prabagaranpersonal@gmail.com>
2022-09-01 17:54:00 +0530

    remove unwanted module imports

--- movie-club-ui/src/shared/ng-zorro-ant-module.ts
+++ movie-club-ui/src/shared/ng-zorro-ant-module.ts
@@ -1,42 +1,12 @@
 
 import { NgModule } from '@angular/core';
-
 import { NzAutocompleteModule } from 'ng-zorro-antd/auto-complete';
 import { NzAnchorModule } from 'ng-zorro-antd/anchor';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 
-// import { NzAvatarModule } from 'ng-zorro-antd/avatar';
-// import { NzButtonModule } from 'ng-zorro-antd/button';
-
-// import { NzDividerModule } from 'ng-zorro-antd/divider';
-// import { NzDrawerModule } from 'ng-zorro-antd/drawer';
-// import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
-// import { NzIconModule } from 'ng-zorro-antd/icon';
-// import { NzInputModule } from 'ng-zorro-antd/input';
-// import { NzLayoutModule } from 'ng-zorro-antd/layout';
-// import { NzListModule } from 'ng-zorro-antd/list';
-// import { NzMenuModule } from 'ng-zorro-antd/menu';
-// import { NzModalModule } from 'ng-zorro-antd/modal';
-// import { NzNotificationModule } from 'ng-zorro-antd/notification';
-// import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
-// import { NzPaginationModule } from 'ng-zorro-antd/pagination';
-// import { NzResultModule } from 'ng-zorro-antd/result';
-// import { NzTreeModule } from 'ng-zorro-antd/tree';
-// import { NzTreeViewModule } from 'ng-zorro-antd/tree-view';
-// import { NzTreeSelectModule } from 'ng-zorro-antd/tree-select';
-// import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
-// import { NzFormModule } from 'ng-zorro-antd/form';
-// import { NzCardModule } from 'ng-zorro-antd/card';
-// import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
-// import { NzCarouselModule } from 'ng-zorro-antd/carousel';
-// import { NzAffixModule } from 'ng-zorro-antd/affix';
-// import { NzStepsModule } from 'ng-zorro-antd/steps';
-// import { NzTabsModule } from 'ng-zorro-antd/tabs';
-
 @NgModule({
   exports: [
-
     NzAutocompleteModule,
     NzCardModule,
     NzLayoutModule,
